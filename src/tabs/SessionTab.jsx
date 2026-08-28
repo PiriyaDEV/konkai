7a7ddaf760acef7e2bf44351f82c6e0a1ea84ecf
Piriya Chaigul <piriya.chaigul@ibm.com>
2026-08-28 17:40:17 +0700

    Re-apply stacked date/court-number fields (regressed in working tree)

--- src/tabs/SessionTab.jsx
+++ src/tabs/SessionTab.jsx
@@ -20,15 +20,13 @@ export default function SessionTab({ session, onField, onCourtStep, onSetPoints,
           <label>{t("fieldLocation")}</label>
           <input type="text" placeholder={t("placeholderLocation")} value={session.location} onChange={handleInput("location")} />
         </div>
-        <div className="row">
-          <div className="field">
-            <label>{t("fieldDate")}</label>
-            <input type="date" value={session.date} onChange={handleInput("date")} />
-          </div>
-          <div className="field">
-            <label>{t("fieldCourtNumbers")}</label>
-            <input type="text" placeholder={t("placeholderCourtNumbers")} value={session.courtNumbers} onChange={handleInput("courtNumbers")} />
-          </div>
+        <div className="field">
+          <label>{t("fieldDate")}</label>
+          <input type="date" value={session.date} onChange={handleInput("date")} />
+        </div>
+        <div className="field">
+          <label>{t("fieldCourtNumbers")}</label>
+          <input type="text" placeholder={t("placeholderCourtNumbers")} value={session.courtNumbers} onChange={handleInput("courtNumbers")} />
         </div>
         <div className="field">
           <label>{t("fieldCourtCount")}</label>
